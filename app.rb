6d1efba641951eeed01e3b1ffaa3af5e2b664ccc
Andrew Rogers <andrewprogers@gmail.com>
2017-06-12 17:07:01 -0400

    Complete optional challenges

--- app.rb
+++ app.rb
@@ -1,25 +1,38 @@
-require "sinatra"
-require_relative "models/player"
-require_relative "models/team"
-require_relative "models/team_data"
+require 'sinatra'
+require_relative 'models/player'
+require_relative 'models/team'
+require_relative 'models/team_data'
 require 'pry'
 
 set :bind, '0.0.0.0'  # bind to all interfaces
 
 
-get "/" do
-  "<h1>The LACKP Homepage<h1>"
+get '/' do
+  erb :home
 end
 
-get "/teams/?" do
+get '/teams/?' do
   @team_names = Team.all.map { |team| team.name }
 
   erb :teams
 end
 
-get "/teams/:team_name" do
+get '/teams/:team_name' do
   @team = Team.all.find { |t| t.name == params[:team_name]}
   @players = @team.players
-  
+
   erb :team_players
+end
+
+get '/positions/?' do
+  @positions = Player.all.map { |player| player.position }.uniq
+
+  erb :positions
+end
+
+get '/positions/:position_name' do
+  @position = params[:position_name]
+  @players = Player.all.select { |player| player.position == @position }
+
+  erb :unique_position
 end
