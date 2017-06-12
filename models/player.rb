c9c8974ce7c369239855268d2251a7e7af71c90f
Andrew Rogers <andrewprogers@gmail.com>
2017-06-12 16:35:02 -0400

    refactor model classes

--- models/player.rb
+++ models/player.rb
@@ -3,26 +3,20 @@ require_relative "./team_data"
 class Player
   attr_reader :name, :position, :team_name
 
-  @@players = []
 
   def initialize(name, position, team_name)
     @name = name
     @position = position
     @team_name = team_name
-    @@players << self
   end
 
   def self.all
-    @@players
-  end
-
-  def self.parse
+    players = []
     TeamData::ROLL_CALL.each do |team_name, roster|
       roster.each do |position, player_name|
-        Player.new(player_name.to_s, position.to_s, team_name.to_s)
+        players << Player.new(player_name.to_s, position.to_s, team_name.to_s)
       end
     end
+    players
   end
 end
-
-Player.parse
--- models/team.rb
+++ models/team.rb
@@ -4,15 +4,16 @@ require_relative "player"
 class Team
   attr_reader :name
 
-  @@teams = []
-
   def initialize(name)
     @name = name
-    @@teams << self
   end
 
   def self.all
-    @@teams
+    teams = []
+    TeamData::ROLL_CALL.each do |team_name, roster|
+      teams << Team.new(team_name.to_s)
+    end
+    teams
   end
 
   def players
@@ -20,12 +21,4 @@ class Team
       player.team_name == @name
     end
   end
-
-  def self.parse
-    TeamData::ROLL_CALL.each do |team_name, roster|
-      Team.new(team_name.to_s)
-    end
-  end
 end
-
-Team.parse
