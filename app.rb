57f8d3645a20522972bd403c743f67f55c7acf37
Cartwright Foreman <cartwrightforeman@Cartwrights-MacBook-Pro.local>
2017-06-12 14:59:04 -0400

    started teams.erb

--- app.rb
+++ app.rb
@@ -9,3 +9,15 @@ set :bind, '0.0.0.0'  # bind to all interfaces
 get "/" do
   "<h1>The LACKP Homepage<h1>"
 end
+
+get "/teams/?" do
+  @team_names = Team.all.map { |team| team.name }
+
+  erb :teams
+end
+
+get "/teams/:team_name" do
+  @team_name = params[:team_name]
+  teams = Teams.all
+  
+end
